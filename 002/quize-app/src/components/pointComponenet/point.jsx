0e2d6fe94384ea5cc31f8e5e8fe62572c05b009d
Hossein Sadeghi <141355464+Ihosseinsd@users.noreply.github.com>
2024-08-14 12:26:56 +0330

    Delete 002/quize-app directory

--- 002/quize-app/src/components/pointComponenet/point.jsx
+++
@@ -1,9 +0,0 @@
-import "./index.css"
-const ShowPoint = (props) => {
-    return(
-        <>
-        <h1>{"Your Point is" + " " + props.thePoint}</h1>
-        </>
-    )
- }
- export default ShowPoint;
